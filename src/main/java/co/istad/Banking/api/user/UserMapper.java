9e1c85876f60123afe7a7b949830f62a5731bacd
Nun-SomRithy <nunsomrithy098@gmail.com>
2023-04-26 13:57:56 +0700

    Create User  Dto👍👍👍

--- src/main/java/co/istad/Banking/api/user/UserMapper.java
+++ src/main/java/co/istad/Banking/api/user/UserMapper.java
@@ -1,17 +1,15 @@
 package co.istad.Banking.api.user;
 
 
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
 public interface UserMapper {
 
-
     @InsertProvider(type = UserProvider.class,method ="buildInsertSql")
+    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
     void insert(@Param("u") User user);
 
 
--- src/main/java/co/istad/Banking/api/user/UserServiceImpl.java
+++ src/main/java/co/istad/Banking/api/user/UserServiceImpl.java
@@ -3,9 +3,11 @@ package co.istad.Banking.api.user;
 import co.istad.Banking.api.user.web.CreateUserDto;
 import co.istad.Banking.api.user.web.UserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
@@ -17,6 +19,7 @@ public class UserServiceImpl implements UserService {
     public UserDto createNewUser(CreateUserDto createUserDto) {
         User user=userMapStruct.createUserDtoToUser(createUserDto);
         userMapper.insert(user);
+        log.info("User id =" + user.getId());
         return userMapStruct.userToUserDto(user);
     }
 }
